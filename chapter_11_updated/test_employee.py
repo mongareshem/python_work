4fc1b6eed1a20e722ced2604791e54115327b372
Shem <mongareshem1@gmail.com>
2026-07-26 23:57:45 +0300

    Test if a custom raise works

--- chapter_11_updated/test_employee.py
+++ chapter_11_updated/test_employee.py
@@ -4,3 +4,8 @@ def test_give_default_raise():
     employee = Employee('Shem', 'Samson', 10000)
     employee.give_raise()
     assert employee.annual_salary == 15000
+
+def test_give_custom_raise():
+    employee = Employee('Shem', 'Samson', 10000)
+    employee.give_raise(7_000)
+    assert employee.annual_salary == 17000
