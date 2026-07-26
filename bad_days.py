561ca7664c92ad1a11d80801bd3b3c02ccdf6d61
Shem <mongareshem1@gmail.com>
2026-07-26 10:43:07 +0300

    Create a function to format names

--- bad_days.py
+++
@@ -1,7 +0,0 @@
-# 05/July/2026
-# poster shenanigans
-print(f"I truly and honestly love programming")
-
-# 06/July/2026
-# poster shenanigans
-print(f"REMONTADA!!!")
---
+++ chapter_11_updated/name_function.py
@@ -0,0 +1,4 @@
+def get_formatted_name(firstname, lastname):
+    """Generate a neatly formatted full name"""
+    full_name = f'{firstname} {lastname}'
+    return full_name.title()
