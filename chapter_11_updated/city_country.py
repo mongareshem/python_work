5cfb2d47c0bc5ca7dc2dc0771233dbd61342cdca
Shem <mongareshem1@gmail.com>
2026-07-26 23:43:54 +0300

    Test the function format_city_country_name() with the optional argument

--- chapter_11_updated/city_country.py
+++ chapter_11_updated/city_country.py
@@ -1,5 +1,8 @@
-def format_city_country_name(city, country, population=''):
+def format_city_country_name(city, country, population=0):
     """A function that returns a neatly formatted city and country name"""
-    return f'{city.title()}, {country.title()}'
+    if population:
+        return f'{city.title()}, {country.title()}, population={population}'
+    else:
+        return f'{city.title()}, {country.title()}'
 
-print(format_city_country_name('nairobi', 'kenya'))
+print(format_city_country_name('nairobi', 'kenya', ))
--- chapter_11_updated/test_city_country.py
+++ chapter_11_updated/test_city_country.py
@@ -4,3 +4,9 @@ def test_city_country():
     """Do arguments like 'Santiago, Chile' work?"""
     formatted_name = format_city_country_name('santiago', 'chile')
     assert formatted_name == 'Santiago, Chile'
+
+
+def test_city_country_population():
+    formatted_name = format_city_country_name('santiago', 'chile',
+                                              5_000_000)
+    assert formatted_name == 'Santiago, Chile, population=5000000'
