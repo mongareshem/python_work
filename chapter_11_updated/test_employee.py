16d1d98ad8591a2f0d5a4c00e1e7d2f028fdcae9
Shem <mongareshem1@gmail.com>
2026-07-26 23:59:47 +0300

    Use fixtures

--- chapter_11_updated/test_employee.py
+++ chapter_11_updated/test_employee.py
@@ -1,11 +1,16 @@
+import pytest
 from employee_class import Employee
 
-def test_give_default_raise():
+@pytest.fixture()
+def employee():
     employee = Employee('Shem', 'Samson', 10000)
+    return employee
+
+
+def test_give_default_raise(employee):
     employee.give_raise()
     assert employee.annual_salary == 15000
 
-def test_give_custom_raise():
-    employee = Employee('Shem', 'Samson', 10000)
+def test_give_custom_raise(employee):
     employee.give_raise(7_000)
     assert employee.annual_salary == 17000
