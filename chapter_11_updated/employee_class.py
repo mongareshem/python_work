845ea940ecf2e69ea16f4a67c77c0f1c534cc46f
Shem <mongareshem1@gmail.com>
2026-07-26 23:51:35 +0300

    Write a method that has a default value and accepts a custom value

--- chapter_11_updated/employee_class.py
+++ chapter_11_updated/employee_class.py
@@ -4,3 +4,7 @@ class Employee:
         self.firstname = firstname
         self.lastname = lastname
         self.annual_salary = annual_salary
+
+    def give_raise(self, increment=5_000):
+        self.annual_salary += increment
+        return self.annual_salary
