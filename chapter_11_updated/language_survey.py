b3f07b414b78f9137ff99021dd502abefff35f61
Shem <mongareshem1@gmail.com>
2026-07-26 13:15:58 +0300

    Conduct the survey and print the results

---
+++ chapter_11_updated/language_survey.py
@@ -0,0 +1,17 @@
+from survey import AnonymousSurvey
+
+question = 'What language did you first learn to speak? '
+language_survey = AnonymousSurvey(question)
+
+language_survey.show_question()
+print('Enter "q" at any time to quit\n')
+
+while True:
+    response = input('Language: ')
+    if response == 'q':
+        break
+
+    language_survey.store_response(response)
+
+print('Thank you everyone who participated in the survey!')
+language_survey.show_results()
