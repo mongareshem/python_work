b86642da485ebe3aae39468b9a4ab94a0e9dba54
Shem <mongareshem1@gmail.com>
2026-07-27 22:46:49 +0300

    Write multiple lines to a file

--- chapter_10_updated/write_message.py
+++ chapter_10_updated/write_message.py
@@ -1,4 +1,9 @@
 from pathlib import Path
 
+contents = 'I love programming\n'
+contents += 'I love creating new games\n'
+contents += 'I love working with data\n'
+contents += 'I also love developing web apps'
+
 path = Path('programming.txt')
-path.write_text('I love programming.')
+path.write_text(contents)
