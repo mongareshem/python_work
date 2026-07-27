eb0a2fc02d6c3d15f8ef9b06cf0e24af982b7f30
Shem <mongareshem1@gmail.com>
2026-07-27 21:54:07 +0300

    Split the file contents into lines and print them out

--- chapter_10_updated/file_reader.py
+++ chapter_10_updated/file_reader.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
 path = Path('pi_digits.txt')
-contents = path.read_text().rstrip()
-print(contents)
+contents = path.read_text()
+lines = contents.splitlines()
+
+for line in lines:
+    print(line)
