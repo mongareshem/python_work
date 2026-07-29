afec241ffdce442752a40ebfb66b4852042afda5
Shem <mongareshem1@gmail.com>
2026-07-30 00:19:56 +0300

    Ask for more information from a user and improve the program

--- chapter_10_updated/exercises/remember_me.py
+++ chapter_10_updated/exercises/remember_me.py
@@ -6,16 +6,27 @@ path = Path('remember_me.json')
 user_details = {}
 
 while True:
-    username = input('What is your name? ')
+    username = input('\nWhat is your name? ')
     if username.lower() == 'q':
         break
 
+    unique_numbers = []
     birthday = input('What is your birthday? ')
     if birthday.lower() == 'q':
         break
+    else:
+        unique_numbers.append(birthday)
 
-    user_details[f'{username}'] = birthday
-    user_dict = json.dumps(user_details)
-    path.write_text(user_dict)
+    reg_number = input('What is your reg number? ')
+    if reg_number.lower() == 'q':
+        break
+    else:
+        unique_numbers.append(reg_number)
 
-
+    if f'{username}' not in user_details.keys():
+        user_details[f'{username}'] = unique_numbers
+        user_dict = json.dumps(user_details)
+        path.write_text(user_dict)
+    else:
+        print(f'The username {username} already exists!')
+        continue
