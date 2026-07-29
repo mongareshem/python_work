9eb37a44f66c6e8ff0c206cc595aa63316fef177
Shem <mongareshem1@gmail.com>
2026-07-29 06:06:19 +0300

    Refactor the code so that each function does exactly one task

--- chapter_10_updated/remember_me.py
+++ chapter_10_updated/remember_me.py
@@ -1,14 +1,34 @@
 from pathlib import Path
 import json
 
-path = Path('username.json')
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back {username.title()}!")
+def get_stored_username(path):
+    """Get stored username if available"""
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
 
-else:
+
+def get_new_username(path):
+    """Prompt for a new username"""
     username = input('What is your name? ')
     contents = json.dumps(username)
     path.write_text(contents)
-    print(f"We'll try to remember you when you come back, {username.title()}!")
+    return username
+
+
+def greet_user():
+    """Greet the user by name"""
+    path = Path('username.json')
+
+    username = get_stored_username(path)
+    if username:
+        print(f"Welcome back {username.title()}!") # type: ignore
+    else:
+        username = get_new_username(path)
+        print(f"We'll try to remember you when you come back, {username.title()}!")
+
+
+greet_user()
